46abb6fa9ed4bcc5093fdf82934a2d6f616a6208
toor <k.nosek@ievent-management.pl>
2019-07-16 22:05:51 +0000

    first playing on game

--- XOGame.py
+++ XOGame.py
@@ -22,8 +22,40 @@ round = 1
 
 
 while round < 10:
-    if x == 2 or y == 2:
-        while round % 2 != 0:
+    while round % 2 != 0:
+        if field_game[0:3] == ["o", "o", "o"]:
+            print("player2 win")
+            round = 20
+            break
+        elif field_game[3:6] == ["o", "o", "o"]:
+            print("player2 win")
+            round = 20
+            break
+        elif field_game[6:9] == ["o", "o", "o"]:
+            print("player2 win")
+            round = 20
+            break
+        elif field_game[0] == "o" and field_game[3] == "o" and field_game[6] == "o":
+            print("player2 win")
+            round = 20
+            break
+        elif field_game[1] == "o" and field_game[4] == "o" and field_game[7] == "o":
+            print("player2 win")
+            round = 20
+            break
+        elif field_game[2] == "o" and field_game[5] == "o" and field_game[8] == "o":
+            print("player2 win")
+            round = 20
+            break
+        elif field_game[0] == "o" and field_game[4] == "o" and field_game[8] == "o":
+            print("player2 win")
+            round = 20
+            break
+        elif field_game[2] == "o" and field_game[4] == "o" and field_game[6] == "o":
+            print("player2 win")
+            round = 20
+            break
+        else:
             print(field_game[0], field_game[1], field_game[2])
             print(field_game[3], field_game[4], field_game[5])
             print(field_game[6], field_game[7], field_game[8])
@@ -48,50 +80,67 @@ while round < 10:
                 field_game[7] = "x"
             if num == 9:
                 field_game[8] = "x"
-        while round % 2 == 0:
-            if round < 10:
-                if field_game[0:3] == ["x", "x", "x"]:
-                    print("player1 win")
-                    round = 20
-                    break
-                elif field_game[3:6] == ["x", "x", "x"]:
-                    print("player1 win")
-                    round = 20
-                    break
-                elif field_game[6:9] == ["x", "x", "x"]:
-                    print("player1 win")
-                    round = 20
-                    break
-                else:
-                    print(field_game[0], field_game[1], field_game[2])
-                    print(field_game[3], field_game[4], field_game[5])
-                    print(field_game[6], field_game[7], field_game[8])
-                    round += 1
-                    num = input("player2 ")
-                    num = int(num)
-                    if num == 1:
-                        field_game[0] = "o"
-                    if num == 2:
-                        field_game[1] = "o"
-                    if num == 3:
-                        field_game[2] = "o"
-                    if num == 4:
-                        field_game[3] = "o"
-                    if num == 5:
-                        field_game[4] = "o"
-                    if num == 6:
-                        field_game[5] = "o"
-                    if num == 7:
-                        field_game[6] = "o"
-                    if num == 8:
-                        field_game[7] = "o"
-                    if num == 9:
-                        field_game[8] = "o"
-            else:
+    while round % 2 == 0:
+        if round < 10:
+            if field_game[0:3] == ["x", "x", "x"]:
+                print("player1 win")
+                round = 20
                 break
-    else:
-        print("player2 win")
-        break
+            elif field_game[3:6] == ["x", "x", "x"]:
+                print("player1 win")
+                round = 20
+                break
+            elif field_game[6:9] == ["x", "x", "x"]:
+                print("player1 win")
+                round = 20
+                break
+            elif field_game[0] == "x" and field_game[3] == "x" and field_game[6] == "x":
+                print("player1 win")
+                round = 20
+                break
+            elif field_game[1] == "x" and field_game[4] == "x" and field_game[7] == "x":
+                print("player1 win")
+                round = 20
+                break
+            elif field_game[2] == "x" and field_game[5] == "x" and field_game[8] == "x":
+                print("player1 win")
+                round = 20
+                break
+            elif field_game[0] == "x" and field_game[4] == "x" and field_game[8] == "x":
+                print("player1 win")
+                round = 20
+                break
+            elif field_game[2] == "x" and field_game[4] == "x" and field_game[6] == "x":
+                print("player1 win")
+                round = 20
+                break
+            else:
+                print(field_game[0], field_game[1], field_game[2])
+                print(field_game[3], field_game[4], field_game[5])
+                print(field_game[6], field_game[7], field_game[8])
+                round += 1
+                num = input("player2 ")
+                num = int(num)
+                if num == 1:
+                    field_game[0] = "o"
+                if num == 2:
+                    field_game[1] = "o"
+                if num == 3:
+                    field_game[2] = "o"
+                if num == 4:
+                    field_game[3] = "o"
+                if num == 5:
+                    field_game[4] = "o"
+                if num == 6:
+                    field_game[5] = "o"
+                if num == 7:
+                    field_game[6] = "o"
+                if num == 8:
+                    field_game[7] = "o"
+                if num == 9:
+                    field_game[8] = "o"
+        else:
+            break
 else:
     print("it's tie")
 
--- test3.py
+++ test3.py
@@ -3,7 +3,11 @@ field_game = ["x", "x", "x",
               4, 5, 6,
               7, 8, 9]
 
-print(field_game[3:6])
+
+rows1 = ''
+for e in str(field_game):
+    rows1 += e
+    print(rows1)
 
 if field_game[0:3] == ["x", "x", "x"]:
     print("brawo")
